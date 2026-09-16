b350b47a8dfe152a7e0091978a92f2aa4357f46d
ensaf01 <mohammedensaf402@gmail.com>
2026-09-16 21:52:54 +0600

    pass and name

--- coffee-card-client-side/src/App.jsx
+++ coffee-card-client-side/src/App.jsx
@@ -1,11 +1,12 @@
 
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
 
 function App() {
-
+  const coffeesx=useLoaderData();
   return (
     <>
-      <h1>COffee Card page</h1>
+      <h1>COffee Card length:{coffeesx.length}</h1>
       
     </>
   )
